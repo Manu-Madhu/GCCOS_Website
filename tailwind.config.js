030ceb91d50fd2149896abb53ac5b8d2e3f28d3d
Akash M Nandan <akash003nandan@gmail.com>
2024-10-03 23:41:46 +0530

    custom colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green-4':'#0d2620',
+        'green-3':'#445855',
+        'green-2':'#07837a',
+        'green-1':'#d8ead8',
+        'white' : '#ffffff',
+        'black' : '#000000',
+      }
+    },
   },
   plugins: [],
 }
