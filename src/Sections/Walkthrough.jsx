f2d72b9cb292e462a81195a1f0cae7d1d8e71791
Akash M Nandan <akash003nandan@gmail.com>
2024-10-04 02:00:42 +0530

    walkthrough css added

--- src/Sections/Walkthrough.jsx
+++ src/Sections/Walkthrough.jsx
@@ -1,8 +1,17 @@
 import gif from '../assets/Gifs/walkthrough.mp4'
+import logoBlack from '../assets/Images/Logo-black.webp'
 
 const Walkthrough = () => {
   return (
-    <video src={gif} autoPlay muted loop className='lg:h-screen lg:w-full'/>
+    <>
+        <video src={gif} autoPlay muted loop className='lg:h-screen lg:w-[120%] z-0'/>
+        <div className='flex flex-col items-center -mt-[600px] z-10 gap-6 text-green-4'>
+            <img src={logoBlack} className='h-56' />
+            <p className='text-[4.2rem] font-semibold'>Geojit CUSAT Center Of Sustainability Studies</p>
+            <p className='text-6xl font-semibold'>GCCOSS</p>
+        </div>
+    </>
+    
   )
 }
 
