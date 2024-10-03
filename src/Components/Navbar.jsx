fbad8d24e026645fb99b2362a96e77a343319f08
Akash M Nandan <akash003nandan@gmail.com>
2024-10-04 00:00:01 +0530

    navbar layout created

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,21 @@
+import logo from '../assets/Images/Logo.webp'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="lg:w-full lg:h-32 bg-green-4 lg:px-14 lg:py-5">
+        <div className="lg:h-full lg:w-full bg-green-3 lg:rounded-2xl">
+            <div className="class">
+                <div>
+                    <a href="">Home</a>
+                    <a href="">About</a>
+                    <a href="">Gallery</a>
+                    <a href="">Blogs</a>
+                </div>
+                <img src={logo} alt="" />
+                <button className="class">User Profile</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
