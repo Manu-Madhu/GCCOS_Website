66f82f8dd95472c12cdb4897f4e2ce5f351ab7d9
Akash M Nandan <akash003nandan@gmail.com>
2024-10-04 02:29:45 +0530

    custom animation set

--- src/Sections/Walkthrough.jsx
+++ src/Sections/Walkthrough.jsx
@@ -9,8 +9,8 @@ const Walkthrough = () => {
         <video src={gif} autoPlay muted loop className='lg:h-screen lg:w-[120%] z-0'/>
         <div className='flex flex-col items-center -mt-[600px] z-10 gap-6 text-green-4'>
             <img src={logoBlack} className='appear h-56' />
-            <p className='appear text-[4.2rem] font-semibold'>Geojit CUSAT Center Of Sustainability Studies</p>
-            <p className='appear text-6xl font-semibold'>GCCOSS</p>
+            <p className='appear text-[4rem] font-semibold'>Geojit CUSAT Center Of Sustainability Studies</p>
+            <p className='appear text-8xl font-semibold'>GCCOSS</p>
         </div>
     </>
     
