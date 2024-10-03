092045c7f3a918f02ee356128c2b5a1fbfd046ff
Akash M Nandan <akash003nandan@gmail.com>
2024-10-04 02:23:25 +0530

    custom animation set

--- src/Sections/Walkthrough.jsx
+++ src/Sections/Walkthrough.jsx
@@ -1,14 +1,16 @@
 import gif from '../assets/Gifs/walkthrough.mp4'
 import logoBlack from '../assets/Images/Logo-black.webp'
 
+import '../Styles/AppearOnScroll.css'
+
 const Walkthrough = () => {
   return (
     <>
         <video src={gif} autoPlay muted loop className='lg:h-screen lg:w-[120%] z-0'/>
         <div className='flex flex-col items-center -mt-[600px] z-10 gap-6 text-green-4'>
-            <img src={logoBlack} className='h-56' />
-            <p className='text-[4.2rem] font-semibold'>Geojit CUSAT Center Of Sustainability Studies</p>
-            <p className='text-6xl font-semibold'>GCCOSS</p>
+            <img src={logoBlack} className='appear h-56' />
+            <p className='appear text-[4.2rem] font-semibold'>Geojit CUSAT Center Of Sustainability Studies</p>
+            <p className='appear text-6xl font-semibold'>GCCOSS</p>
         </div>
     </>
     
