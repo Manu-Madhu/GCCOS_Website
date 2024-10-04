390aedeec3bed4b0a041fb07e6684ba42e05c31b
Akash M Nandan <akash003nandan@gmail.com>
2024-10-04 16:46:11 +0530

    banner changed in hero section

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,9 +1,9 @@
-import gif from '../assets/Gifs/hero2.gif'
+import gif from '../assets/Gifs/banner.mp4'
 
 const Hero = () => {
   return (
     <div className="flex flex-col h-screen">
-      <img src={gif} alt="" className="h-[750px] w-full items-center justify-center bg-cover bg-center z-0"/>
+      <video src={gif} muted autoPlay loop className="items-center justify-center bg-cover bg-center z-0"/>
       <p className='lg:ml-20 lg:text-9xl text-white lg:font-semibold z-10 lg:-mt-[660px] tracking-tight'>Empowering dreams <br /> of a greener world</p>
     </div>
   )
