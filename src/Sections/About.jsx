8d92bc06cded39ff90c4ef5ac55082dfc0ae82bd
Akash M Nandan <akash003nandan@gmail.com>
2024-10-04 07:55:04 +0530

    about content set

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -6,6 +6,7 @@ const About = () => {
     <div className="lg:mt-[136px] lg:h-[2000px] lg:w-full bg-green-4 pt-60">
         <p className="lg:text-9xl text-white">Get To Know About <br /> Us</p>
         <img src={about0} alt="" className='appear lg:w-[1400px] lg:ml-[170px] lg:-mt-[100px]'/>
+        <p className='lg:h-[700px] lg:w-[1000px] lg:ml-[500px] bg-green-1 lg:text-3xl p-7 text-justify lg:leading-[70px]'>GCCOSS is a collaborative initiative between Cochin University of Science and Technology (CUSAT) and Geojit Financial Services Limited (Geojit). This joint venture aims to promote sustainability, support the ecosystem, and serve as a facilitator for companies, government bodies, investors, and individuals in their Environmental, Social, and Governance (ESG) endeavours.</p>
     </div>
   )
 }
