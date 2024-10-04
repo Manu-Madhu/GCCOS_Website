c6ce58e0de19c0487b9b50eb14039c9c7bedc798
Akash M Nandan <akash003nandan@gmail.com>
2024-10-04 18:37:59 +0530

    button added in testimonials section

--- src/Sections/Testimonials.jsx
+++ src/Sections/Testimonials.jsx
@@ -1,6 +1,6 @@
 import '../Styles/AppearOnScroll.css'
 import '../Styles/InfiniteScroll.css'
-
+import Typewriter from 'typewriter-effect';
 
 const Testimonials = () => {
   return (
@@ -20,6 +20,27 @@ const Testimonials = () => {
                 <p className=''>in Community</p>
             </div>
         </div>
+        <a href="#" className="ml-[1250px] flex flex-row bg-white border-black border-2 p-2 rounded-xl items-center justify-center max-sm:mt-0 max-sm:text-xs gap-3 pointer-events-auto hover:bg-green-2 hover:text-white duration-300">
+            <p className="tracking-widest max-sm:tracking-wider">
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter.typeString('Come Explore with Us')
+                    .pauseFor(1000)
+                    .deleteAll()
+                    .typeString('Share Your Thoughts')
+                    .pauseFor(1000)
+                    .deleteAll()
+                    .typeString('Pass the meassage')
+                    .pauseFor(1000)
+                    .start();
+                }}
+                options={{
+                  delay: 150,
+                  loop: true,
+                }}
+              />
+            </p>
+          </a>
     </div>
   )
 }
