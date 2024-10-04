44b493a3d0f39a5e6ba23ba13660a787c4b300e2
Akash M Nandan <akash003nandan@gmail.com>
2024-10-04 13:18:41 +0530

    bamboo texture set

--- src/Sections/Blogs.jsx
+++ src/Sections/Blogs.jsx
@@ -1,9 +1,13 @@
+import bg from '../assets/Images/blogs.jpg'
 
 
 const Blogs = () => {
   return (
-    <div className="lg:h-screen bg-green-4 p-16">
-        <p className="lg:text-8xl font-medium text-white">Blogs</p>
+    <div className="lg:h-screen bg-green-4 flex flex-col gap-7">
+        <p className="lg:text-8xl font-medium lg:mt-10 lg:ml-10 text-white">Blogs</p>
+        <div className='w-full mt-14'>
+            <img src={bg} className='h-[535px] w-[1700px]'/>
+        </div>
     </div>
   )
 }
