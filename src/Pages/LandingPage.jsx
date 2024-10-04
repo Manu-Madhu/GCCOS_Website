550b1ad5977ddc31527befa7068510ec8a44eefc
Akash M Nandan <akash003nandan@gmail.com>
2024-10-04 12:38:44 +0530

    blog heading

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../Components/Navbar"
 import About from "../Sections/About"
 import About2 from "../Sections/About2"
+import Blogs from "../Sections/Blogs"
 import Directors from "../Sections/Directors"
 import Gallery from "../Sections/Gallery"
 import Gallery_pics from "../Sections/Gallery_pics"
@@ -18,6 +19,7 @@ const LandingPage = () => {
         <Gallery/>
         <Gallery_pics/>
         <Directors/>
+        <Blogs/>
     </>
   )
 }
---
+++ src/Sections/Blogs.jsx
@@ -0,0 +1,11 @@
+
+
+const Blogs = () => {
+  return (
+    <div className="lg:h-screen bg-green-4 p-16">
+        <p className="lg:text-8xl font-medium text-white">Blogs</p>
+    </div>
+  )
+}
+
+export default Blogs
