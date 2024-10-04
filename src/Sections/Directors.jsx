65056084a329132ed9bca3c8089cf6213a80b3f9
Akash M Nandan <akash003nandan@gmail.com>
2024-10-04 11:30:07 +0530

    animate added on components

--- src/Sections/Directors.jsx
+++ src/Sections/Directors.jsx
@@ -3,12 +3,14 @@ import dir2 from '../assets/Images/dir2.webp'
 import dir3 from '../assets/Images/dir3.webp'
 import dir4 from '../assets/Images/dir4.webp'
 
+import '../Styles/AppearOnScroll.css'
+
 const Directors = () => {
   return (
     <div className="lg:h-auto lg:pb-10 bg-[#77846d] lg:px-10 lg:pt-6 text-white">
-        <p className='text-8xl font font-medium'>Our Directors</p>
+        <p className='appear text-8xl font font-medium'>Our Directors</p>
         <div className='lg:mt-16 flex flex-row gap-4 items-end justify-center'>
-            <div className='flex flex-col'>
+            <div className='appear flex flex-col'>
                 <img src={dir1} alt="" className='lg:h-[450px] lg:w-[350px] rounded-3xl' />
                 <div className='lg:mt-8 flex flex-col gap-4'>
                     <p className='lg:text-2xl font-medium'>Charlie McMann</p>
@@ -18,7 +20,7 @@ const Directors = () => {
                     </div>
                 </div>
             </div>
-            <div className='flex flex-col'>
+            <div className='appear flex flex-col'>
                 <img src={dir2} alt="" className='lg:h-[300px] lg:w-[300px] rounded-3xl' />
                 <div className='lg:mt-8 flex flex-col gap-4'>
                     <p className='lg:text-2xl font-medium'>Kris Ward</p>
@@ -28,7 +30,7 @@ const Directors = () => {
                     </div>
                 </div>
             </div>
-            <div className='flex flex-col'>
+            <div className='appear flex flex-col'>
                 <img src={dir3} alt="" className='lg:h-[550px] lg:w-[350px] rounded-3xl' />
                 <div className='lg:mt-8 flex flex-col gap-4'>
                     <p className='lg:text-2xl font-medium'>Alex Smith</p>
@@ -38,7 +40,7 @@ const Directors = () => {
                     </div>
                 </div>
             </div>
-            <div className='flex flex-col'>
+            <div className='appear flex flex-col'>
                 <img src={dir4} alt="" className='lg:h-[400px] lg:w-[350px] rounded-3xl' />
                 <div className='lg:mt-8 flex flex-col gap-4'>
                     <p className='lg:text-2xl font-medium'>Reese Whitman</p>
